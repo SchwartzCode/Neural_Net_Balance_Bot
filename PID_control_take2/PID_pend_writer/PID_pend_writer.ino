1b3a84bf773f9e128901b584d4d6e0c600060522
SchwartzCode <jonbs98@gmail.com>
2021-05-30 15:27:47 -0500

    Wasn't a software error, had too many sensors connected to same ground port on Ard.. whoops!

--- PID_control_take2/PID_pend_writer/PID_pend_writer.ino
+++ PID_control_take2/PID_pend_writer/PID_pend_writer.ino
@@ -15,6 +15,8 @@ Adafruit_MPU6050 mpu; //initialize IMU object
 //(1-alpha)*angle_gyro
 #define alpha  0.05 
 
+byte newEncoderData = 0;
+
 // initialize variables
 float angle = 0.0;
 float I_error = 0.0;
@@ -52,65 +54,11 @@ void setup(void) {
   }
   Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
+  
+  // set IMU parameters
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G); // options:  +- 2, 4, 8, 16 [G]
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);      // options:  250, 500, 1000, 2000 [DEG]
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);   // options:  260, 184, 94, 44, 21, 10, 5 [Hz]
   Serial.println("");
   delay(100);
 
@@ -137,6 +85,7 @@ void EncoderInit()
  */
 void readEncoder()
 {
+//  newEncoderData = PIND;
   int Lstate = digitalRead(encoder0pinA);
   if((encoder0PinALast == LOW) && Lstate==HIGH)
   {
@@ -216,6 +165,7 @@ void loop() {
 
   double angle = 0;
   double gyro_x = 0;
+
   readSensors(&gyro_x);
 
   float PID_PWM = 0.0;
@@ -241,6 +191,7 @@ void loop() {
     I_error = 0;
   }
 
-//  Serial.println(wheel_pos);
+//  readEncoder();
+  Serial.println(wheel_pos);
 
 }
